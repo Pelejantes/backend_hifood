fa3a7cc7edf0d19cd301d76947bc6be7b8764589
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-04-09 17:38:23 +0000

    Permissão de Ler e Alterar usuarios criada no permissions

--- ifoodApp/permissions.py
+++ ifoodApp/permissions.py
@@ -1,5 +1,30 @@
 from rest_framework import permissions
 
+# RU -> READ / UPDATE
+class RU_Usuario(permissions.BasePermission):
+    def has_permission(self, request, view):
+        tipo_usuario_id = request.auth_payload.get("tipoUsuarioId") if hasattr(request, 'auth_payload') else None
+        usuarioId = request.auth_payload.get("usuarioId") if hasattr(request, 'auth_payload') else None
+        url_pk = view.kwargs.get('pk')  # Obtém a PK da URL
+        # Verificações de permissão para Admin
+        if tipo_usuario_id == "0":
+            return True  # Admin tem acesso total
+
+        # Verificações de permissão para Comprador
+        if tipo_usuario_id == "1" and url_pk == usuarioId:
+            return True  # Comprador tem acesso a listagem, leitura e criação
+
+        # Verificações de permissão para Entregador
+        if tipo_usuario_id == "2" and url_pk == usuarioId:
+            return True  # Entregador tem acesso a leitura e atualização
+
+        # Verificações de permissão para Estabelecimento
+        if tipo_usuario_id == "3" and url_pk == usuarioId:
+            return True  # Estabelecimento tem acesso a listagem e criação
+
+        # Se não for nenhum dos casos acima, negar acesso
+        return False
+
 
 class Admin(permissions.BasePermission):
     def has_permission(self, request, view):
