1ec401aef0ee9928bc67a0a7050a6fe248b929c2
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-04-09 17:38:33 +0000

    Permissões devidamente atribuídas na view de usuario.

--- ifoodApp/views/usuario_view.py
+++ ifoodApp/views/usuario_view.py
@@ -1,52 +1,52 @@
 from rest_framework.decorators import api_view, permission_classes
-# from ..permissions import Professor, Admin, PodeEditarPerfil,Cadastrado,IsProfessorOrAdmin
+from ..permissions import Admin, Comprador, Entregador, Estabelecimento, RU_Usuario
 from rest_framework.permissions import AllowAny
 from ..controllers import usuario_controller
 
 
 @api_view(["GET"])
-# @permission_classes([IsProfessorOrAdmin])
+@permission_classes([Admin])
 def exibir_usuarios_view(request):
     return usuario_controller.exibir_usuarios(request)
 
 
 @api_view(["GET"])
-# @permission_classes([PodeEditarPerfil])
+@permission_classes([RU_Usuario])
 def exibir_usuario_view(request, pk):
     return usuario_controller.exibir_usuario(request, pk)
 
 
 @api_view(["POST"])
-# @permission_classes([AllowAny])
+@permission_classes([AllowAny])
 def criar_usuarioCompleto_view(request):
     return usuario_controller.criar_usuarioCompleto(request)
 
 
 @api_view(["POST"])
-# @permission_classes([AllowAny])
+@permission_classes([AllowAny])
 def criar_usuario_view(request):
     return usuario_controller.criar_usuario(request)
 
 
 @api_view(["PUT"])
-# @permission_classes([PodeEditarPerfil])
+@permission_classes([Admin])
 def inativar_usuario_view(request, pk):
     return usuario_controller.inativar_usuario(request, pk)
 
 
 @api_view(["PUT"])
-# @permission_classes([Admin])
+@permission_classes([Admin])
 def ativar_usuario_view(request, pk):
     return usuario_controller.ativar_usuario(request, pk)
 
 
 @api_view(["PUT"])
-# @permission_classes([PodeEditarPerfil])
+@permission_classes([RU_Usuario])
 def editar_usuario_view(request, pk):
     return usuario_controller.editar_usuario(request, pk)
 
 
 @api_view(["DELETE"])
-# @permission_classes([Admin])
+@permission_classes([Admin])
 def deletar_usuario_view(request, pk):
     return usuario_controller.deletar_usuario(request, pk)
