e3ccd623d9030a8973126f6cd0bd5847c5242ee4
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-04-10 20:03:05 -0300

    Ajuste na lógica de permissões para cada tipo de usuario.

--- ifoodApp/permissions.py
+++ ifoodApp/permissions.py
@@ -1,25 +1,29 @@
 from rest_framework import permissions
-
+from models import TipoUsuario
 # RU -> READ / UPDATE
 class RU_Usuario(permissions.BasePermission):
     def has_permission(self, request, view):
         tipo_usuario_id = request.auth_payload.get("tipoUsuarioId") if hasattr(request, 'auth_payload') else None
+        try:
+            nomeTipoUsuario = TipoUsuario.get(tipoUsuarioId=tipo_usuario_id).nomeTipoUsuario
+        except AssertionError:
+            return "Tipo de Usuario não foi registrado."
         usuarioId = request.auth_payload.get("usuarioId") if hasattr(request, 'auth_payload') else None
         url_pk = view.kwargs.get('pk')  # Obtém a PK da URL
         # Verificações de permissão para Admin
-        if tipo_usuario_id == "0":
+        if nomeTipoUsuario == "Admin".lower():
             return True  # Admin tem acesso total
 
         # Verificações de permissão para Comprador
-        if tipo_usuario_id == "1" and url_pk == usuarioId:
+        if nomeTipoUsuario == "Comprador".lower() and url_pk == usuarioId:
             return True  # Comprador tem acesso a listagem, leitura e criação
 
         # Verificações de permissão para Entregador
-        if tipo_usuario_id == "2" and url_pk == usuarioId:
+        if nomeTipoUsuario == "Entregador".lower() and url_pk == usuarioId:
             return True  # Entregador tem acesso a leitura e atualização
 
         # Verificações de permissão para Estabelecimento
-        if tipo_usuario_id == "3" and url_pk == usuarioId:
+        if nomeTipoUsuario == "Estabelecimento".lower() and url_pk == usuarioId:
             return True  # Estabelecimento tem acesso a listagem e criação
 
         # Se não for nenhum dos casos acima, negar acesso
