cc89ffcc7d5253e1bd63d27f471d3cb20db2a87e
JV_Damaceno <55356756+Seefrid@users.noreply.github.com>
2024-04-11 23:13:01 +0000

    Ajuste da importação do models no permissions.

--- ifoodApp/permissions.py
+++ ifoodApp/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from models import TipoUsuario
+from .models import TipoUsuario
 # RU -> READ / UPDATE
 class RU_Usuario(permissions.BasePermission):
     def has_permission(self, request, view):
