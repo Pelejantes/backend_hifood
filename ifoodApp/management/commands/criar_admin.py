dece05c148fb87cbe83badca5ba57dfc59e2e6d5
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-04-12 21:21:06 -0300

    Correção dos parametros para criar usuario e tipoUsuario

--- ifoodApp/management/commands/criar_admin.py
+++ ifoodApp/management/commands/criar_admin.py
@@ -4,22 +4,27 @@ from ifoodApp.controllers.tipoUsuario_controller import criar_tipoUsuario
 from ifoodApp.controllers.usuario_controller import criar_usuario
 import os
 
+
 class Command(BaseCommand):
     help = 'Cria um usuário administrador inicial'
 
     def handle(self, *args, **kwargs):
         # Criar tipo de usuario ADMIn
         criar_tipoUsuario({
-            "nomeTipoUsuario": "Admin"
+            "data": {
+                "nomeTipoUsuario": "Admin"
+            }
         })
         # Criar Usuario ADMIN
         if not Usuario.objects.all():
             criar_usuario({
-                "nomeUsu": os.environ.get("DJANGO_SUPERUSER_USERNAME"),
-                "telefoneUsu": os.environ.get("DJANGO_SUPERUSER_TELEFONE"),
-                "cpf": os.environ.get("DJANGO_SUPERUSER_CPF"),
-                "emailUsu": os.environ.get("DJANGO_SUPERUSER_EMAIL"),
-                "tipoUsuarioId": 0
+                "data": {
+                    "nomeUsu": os.environ.get("DJANGO_SUPERUSER_USERNAME"),
+                    "telefoneUsu": os.environ.get("DJANGO_SUPERUSER_TELEFONE"),
+                    "cpf": os.environ.get("DJANGO_SUPERUSER_CPF"),
+                    "emailUsu": os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+                    "tipoUsuarioId": 0
+                }
             })
             self.stdout.write(self.style.SUCCESS(
                 'Usuário administrador criado com sucesso!'))
