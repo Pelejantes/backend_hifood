655b319090400d54ec94302ee5544e546f490a89
Diego Rodrigues Góes <83193074+Diego-goes@users.noreply.github.com>
2024-04-13 20:46:30 +0000

    Correção ao rodar Django.sh; Ajuste de configuração no Docker-Compose na parte env BD

--- django.sh
+++ django.sh
@@ -14,7 +14,14 @@ echo "===================================="
 
 # Chama o comando para criar o usuário administrador
 python manage.py criar_admin
+echo "===================================="
+
+# Coleta todos os arquivos estáticos em um único diretório que o Gunicorn possa acessar.
+python manage.py collectstatic
+echo "===================================="
 
 # Executando o Gunicorn
 echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:$PORT project.wsgi:application
+python manage.py runserver 0.0.0.0:$PORT
+# exec gunicorn --bind 0.0.0.0:$PORT project.wsgi:application
+echo "===================================="
