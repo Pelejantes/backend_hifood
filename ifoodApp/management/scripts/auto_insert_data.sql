252a95f9324be76fd1536352b40785bc6e384a2c
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-04-13 21:24:45 -0300

    Atualização do script que auto insere dados de estabelecimentos

--- ifoodApp/management/scripts/auto_insert_data.sql
+++ ifoodApp/management/scripts/auto_insert_data.sql
@@ -7,17 +7,17 @@ BEGIN
     FOR i IN 1..50 LOOP
         -- Gera um CNPJ único
         cnpj_gerado := LPAD((ROUND(RANDOM() * 99999999999999))::text, 14, '0');
-        WHILE EXISTS (SELECT 1 FROM ifoodApp_estabelecimento WHERE cnpj = cnpj_gerado) LOOP
+        WHILE EXISTS (SELECT 1 FROM "ifoodApp_estabelecimento" WHERE "cnpj" = cnpj_gerado) LOOP
             cnpj_gerado := LPAD((ROUND(RANDOM() * 99999999999999))::text, 14, '0');
         END LOOP;
 
         -- Gera um telefone único
         telefone_gerado := LPAD((ROUND(RANDOM() * 9999999999))::text, 10, '0');
-        WHILE EXISTS (SELECT 1 FROM ifoodApp_estabelecimento WHERE telefoneEstab = telefone_gerado) LOOP
+        WHILE EXISTS (SELECT 1 FROM "ifoodApp_estabelecimento" WHERE "telefoneEstab" = telefone_gerado) LOOP
             telefone_gerado := LPAD((ROUND(RANDOM() * 9999999999))::text, 10, '0');
         END LOOP;
 
-        INSERT INTO Estabelecimento (   nomeEstab, telefoneEstab, imagemEstab, cnpj, emailEstab)
-        VALUES (  'Estabelecimento_' || i, telefone_gerado, NULL, cnpj_gerado, 'estabelecimento_' || i || '@restaurante.com.br');
+        INSERT INTO "ifoodApp_estabelecimento" ("nomeEstab", "telefoneEstab", "cnpj", "emailEstab")
+        VALUES ('Estabelecimento_' || i, telefone_gerado, cnpj_gerado, 'estabelecimento_' || i || '@restaurante.com.br');
     END LOOP;
 END $$;
