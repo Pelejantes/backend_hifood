fe260175d4b9ff83964be264f0210b9358ecb91d
Diego Rodrigues Góes <diego353.goes@gmail.com>
2024-05-05 21:39:24 -0300

    Ajuste das permissões para endereço

--- ifoodApp/views/enderecoEntrega_view.py
+++ ifoodApp/views/enderecoEntrega_view.py
@@ -1,29 +1,29 @@
 from rest_framework.decorators import api_view, permission_classes
-# from ..permissions import Professor, Admin, PodeEditarPerfil,Cadastrado,IsProfessorOrAdmin
+from ..permissions import Admin, Comprador, Entregador, Estabelecimento, RU_Usuario
 from rest_framework.permissions import AllowAny
 from ..controllers import enderecoEntrega_controller
 
 @api_view(["GET"])
-# @permission_classes([IsProfessorOrAdmin])
+@permission_classes([Admin])
 def exibir_enderecosEntrega_view(request):
    return enderecoEntrega_controller.exibir_enderecosEntrega(request)
 
 @api_view(["GET"])
-# @permission_classes([PodeEditarPerfil])
+@permission_classes([AllowAny])
 def exibir_enderecoEntrega_view(request, pk):
    return enderecoEntrega_controller.exibir_enderecoEntrega(request,pk)
 
 @api_view(["POST"])
-# @permission_classes([AllowAny])
+@permission_classes([AllowAny])
 def criar_enderecoEntrega_view(request):
    return enderecoEntrega_controller.criar_enderecoEntrega(request)
 
 @api_view(["PUT"])
-# @permission_classes([PodeEditarPerfil])
+@permission_classes([AllowAny])
 def editar_enderecoEntrega_view(request, pk):
    return enderecoEntrega_controller.editar_enderecoEntrega(request,pk)
 
 @api_view(["DELETE"])
-# @permission_classes([Admin])
+@permission_classes([AllowAny])
 def deletar_enderecoEntrega_view(request, pk):
    return enderecoEntrega_controller.deletar_enderecoEntrega(request,pk)
--- ifoodApp/views/endereco_view.py
+++ ifoodApp/views/endereco_view.py
@@ -9,21 +9,21 @@ def exibir_enderecos_view(request):
    return endereco_controller.exibir_enderecos(request)
 
 @api_view(["GET"])
-@permission_classes([RU_Usuario])
+@permission_classes([AllowAny])
 def exibir_endereco_view(request, pk):
    return endereco_controller.exibir_endereco(request,pk)
 
 @api_view(["POST"])
-@permission_classes([RU_Usuario])
+@permission_classes([AllowAny])
 def criar_endereco_view(request):
    return endereco_controller.criar_endereco(request)
 
 @api_view(["PUT"])
-@permission_classes([RU_Usuario])
+@permission_classes([AllowAny])
 def editar_endereco_view(request, pk):
    return endereco_controller.editar_endereco(request,pk)
 
 @api_view(["DELETE"])
-@permission_classes([RU_Usuario])
+@permission_classes([AllowAny])
 def deletar_endereco_view(request, pk):
    return endereco_controller.deletar_endereco(request,pk)
